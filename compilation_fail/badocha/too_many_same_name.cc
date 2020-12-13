e1f2b159d185f9eef77ac70a2cd2c075a7ad1a97
a.Ryski <ar426383@students.mimuw.edu.pl>
2020-12-13 13:22:18 +0100

    poprawa too_many

--- compilation_fail/badocha/too_many_same_name.cc
+++ compilation_fail/badocha/too_many_same_name.cc
@@ -6,5 +6,5 @@ int main() {
     using test_machine = Computer<4, int>;
 
     using too_many_same_name = Program<D<Id("a"), Num<42>>, D<Id("a"), Num<42>>, D<Id("a"), Num<42>>, D<Id("a"), Num<42>>, D<Id("a"), Num<42>>>;
-    test_machine::boot<too_many_same_name>();
+    constexpr auto c = test_machine::boot<too_many_same_name>();
 }
